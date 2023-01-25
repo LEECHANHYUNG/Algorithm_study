a0cb2348dd3eec8ac81fa483936245ec30d1780d
LEECHANHYUNG <lsj721538@gmail.com>
2023-01-26 00:18:19 +0900

    baekjoon 2606

---
+++ dfs/2606.바이러스/바이러스.js
@@ -0,0 +1,97 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  DFSPreOrder() {
+    var data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+}
+
+let tree = new BinarySearchTree();
+let input = ['1 2', '2 3', '5 6', '4 7', '1 5', '5 2'];
+let computerList = [];
+input.map((elem) => computerList.push(elem.split(' ')));
+computerList = computerList.map((elem) => elem.map((v) => +v));
+
+let isPut = true;
+tree.insert(1);
+for (let i = 0; i < input.length; i++) {
+  computerList.map((elem) => {
+    if (tree.contains(elem[0])) tree.insert(elem[1]);
+    else if (tree.contains(elem[1])) tree.insert(elem[0]);
+    else isPut = false;
+  });
+}
+console.log(tree);
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPreOrder().length - 1);
